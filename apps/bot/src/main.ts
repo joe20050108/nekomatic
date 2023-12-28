54a7ac813a3fc9038697d30e5c66b0b94e9b0f11
Junior <42356930+joe20050108@users.noreply.github.com>
2023-12-27 22:41:04 -0600

    Added Swagger Docs

--- apps/bot/src/main.ts
+++ apps/bot/src/main.ts
@@ -8,6 +8,8 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -17,6 +19,14 @@ async function bootstrap() {
         : ['log', 'warn', 'error'],
   });
   app.enableShutdownHooks();
+
+  const config = new DocumentBuilder()
+    .setTitle('Bot API Documentation')
+    .setDescription('The documentation for the bot API')
+    .setVersion('current')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
