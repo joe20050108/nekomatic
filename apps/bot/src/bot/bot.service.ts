46254f2bde7a9fada5faf47fa7d5d559432ee9f7
Junior <42356930+joe20050108@users.noreply.github.com>
2023-12-28 14:22:27 -0600

    Added getBot() function to bot.service
    This function gets the Bot from the BOT_URL using axios.get.

--- apps/bot/src/bot/bot.service.ts
+++ apps/bot/src/bot/bot.service.ts
@@ -1,3 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Bot } from '@tf2-automatic/bot-data';
+import { Config } from '../common/config/configuration';
+import axios from 'axios';
 @Injectable()
-export class BotService {}
+export class BotService {
+  private readonly logger = new Logger(BotService.name);
+  constructor(private readonly configService: ConfigService<Config>) {}
+  async getBot(): Promise<Bot> {
+    return await axios
+      .get(`${this.configService.getOrThrow<string>('botUrl')}/bot`)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((err) => {
+        this.logger.error(err);
+        return err;
+      });
+  }
+}
