f6239d54c80aa9fd1df29ee2eb9824f29033beb0
Junior <42356930+joe20050108@users.noreply.github.com>
2023-12-29 14:38:59 -0600

    Added a Necord based Discord bot.

--- apps/bot/src/app.module.ts
+++ apps/bot/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import configuration from './common/config/configuration';
 import { validation } from './common/config/validation';
 import { BotModule } from './bot/bot.module';
+import { DiscordModule } from './discord/discord.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { BotModule } from './bot/bot.module';
       validationSchema: validation,
     }),
     BotModule,
+    DiscordModule,
   ],
 })
 export class AppModule {}
---
+++ apps/bot/src/discord/discord.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { DiscordService } from './discord.service';
+import { NecordModule } from 'necord';
+import { ConfigService } from '@nestjs/config';
+import { Config } from '../common/config/configuration';
+import { GatewayIntentBits } from 'discord.js';
+
+@Module({
+  imports: [
+    NecordModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<Config>) => {
+        const token = configService.getOrThrow<string>('discordBotToken');
+        return {
+          token,
+          intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.DirectMessages,
+            GatewayIntentBits.MessageContent,
+          ],
+          prefix: '!',
+        };
+      },
+    }),
+  ],
+  providers: [DiscordService],
+})
+export class DiscordModule {}
