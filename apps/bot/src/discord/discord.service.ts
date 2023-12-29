941e97b5cc0e8f36e8c4f3ac3cc402e695b31fa5
Junior <42356930+joe20050108@users.noreply.github.com>
2023-12-29 15:08:31 -0600

    create a DM with the operator on Discord

--- apps/bot/src/discord/discord.service.ts
+++ apps/bot/src/discord/discord.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Config, Operator } from '../common/config/configuration';
+import { Once, Context, ContextOf } from 'necord';
 
 @Injectable()
-export class DiscordService {}
+export class DiscordService {
+  private readonly logger = new Logger(DiscordService.name);
+
+  constructor(private readonly configService: ConfigService<Config>) {}
+
+  @Once('ready')
+  public async onReady(@Context() [client]: ContextOf<'ready'>) {
+    this.logger.log(`Bot logged in as ${client.user.username}`);
+    // Create a DM with the operator's discord ID.
+    await client.users
+      .fetch(this.configService.getOrThrow<Operator>('operator').discord)
+      .then((operatorUser) => {
+        client.users
+          .createDM(operatorUser)
+          .then(() => {
+            this.logger.log(
+              `Created DM channel with operator ${operatorUser.username}`
+            );
+          })
+          .catch((error) => {
+            this.logger.error(
+              'Failed to fetch DM channel with operator:',
+              error
+            );
+          });
+      })
+      .catch((error) => {
+        this.logger.error('Failed to fetch DM channel with operator:', error);
+      });
+  }
+}
